c43881c5b7bba25fe4b7fe91af4bdf9234e038cc
Kristina Gergel <gergel-kristina@mail.ru>
2020-11-26 12:44:30 +0300

    ref #1 minor formatting fix

--- javascript/bubbleSort.js
+++ javascript/bubbleSort.js
@@ -1,6 +1,6 @@
 function bubbleSort (a, n) {
     for (let i = n -1; i > 0; i --) {
-        for (let j = 0; j< n - 1; j ++) {
+        for (let j = 0; j < n - 1; j ++) {
             if (a[j] > a[j+1]) {
                 let t = a[j]
                 a[j] = a[j+1]
