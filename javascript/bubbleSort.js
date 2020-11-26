7e6467f02e02d6331ef86245fbeb3e8df3eaa711
Kristina Gergel <gergel-kristina@mail.ru>
2020-11-26 11:58:28 +0300

    ref #1 code formating change

--- javascript/bubbleSort.js
+++ javascript/bubbleSort.js
@@ -1,10 +1,11 @@
-function bubbleSort (a, n){
-for (let i = n -1; i > 0; i --)
-for (let j = 0; j< n - 1; j ++) 
-if (a[j] > a[j+1]) {
-    let t = a[j]
-    a[j] = a[j+1]
-    a[j+1] = t
-}
-
+function bubbleSort (a, n) {
+    for (let i = n -1; i > 0; i --) {
+        for (let j = 0; j< n - 1; j ++) {
+            if (a[j] > a[j+1]) {
+                let t = a[j]
+                a[j] = a[j+1]
+                a[j+1] = t
+            }
+        }
+    }   
 }
